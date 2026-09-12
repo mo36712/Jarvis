1f9d7544937cac6374f10ad235caa9b783a89a23
mo36712 <memedabi54@gmail.com>
2026-09-12 02:11:54 +0200

    Delete JarvisAI_Windows11/jarvis-desktop directory

--- JarvisAI_Windows11/jarvis-desktop/jarvis/__init__.py
+++
@@ -1,3 +0,0 @@
-"""JarvisAI – privater Desktop-Assistent für Windows."""
-
-__version__ = "0.1.0"
--- JarvisAI_Windows11/jarvis-desktop/main.py
+++
@@ -1,4 +0,0 @@
-from jarvis.ui import run
-
-if __name__ == "__main__":
-    raise SystemExit(run())
